4ed8d821e299e77a20e5306064a5807888f9428d
Andrew Koerber <kerbybit@gmail.com>
2017-12-13 10:00:58 -0600

    Updated to match master branch
    f1a7d74e70e49410faf109f109d1fcda90064bb0

--- src/main/java/com/chattriggers/ctjs/commands/CTCommand.java
+++ src/main/java/com/chattriggers/ctjs/commands/CTCommand.java
@@ -67,9 +67,12 @@ public class CTCommand extends CommandBase {
                 case("reload"):
                 case("load"):
                     TriggerType.GAME_UNLOAD.triggerAll();
-                    CTJS.getInstance().getModuleManager().unload();
+                    TriggerType.WORLD_UNLOAD.triggerAll();
                     ChatLib.chat("&cReloading ct.js scripts...");
                     new Thread(() -> {
+                        for (TriggerType type : TriggerType.values())
+                            type.clearTriggers();
+                        CTJS.getInstance().getModuleManager().unload();
                         CTJS.getInstance().getModuleManager().load();
                         ChatLib.chat("&aDone reloading scripts!");
                         TriggerType.WORLD_LOAD.triggerAll();
